57a47fe3cef1dd31374a4caa11726c8cff188b8f
AbdulRehman2040 <fahadkhan1tea@gmail.com>
2024-07-02 20:42:02 +0500

    h

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="abdul-iamginify"
       options={{
         multiple: false,
         resourceType: "image",
